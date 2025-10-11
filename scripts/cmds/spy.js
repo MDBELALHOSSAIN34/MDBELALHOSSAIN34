b321c2eaa32509b3222d8eedb7729a9326173faf
MDBELALHOSSAIN23 <236202726+MDBELALHOSSAIN23@users.noreply.github.com>
2025-10-12 00:52:14 +0600

    Update spy.js

--- scripts/cmds/spy.js
+++ scripts/cmds/spy.js
@@ -9,7 +9,7 @@ const baseApiUrl = async () => {
 module.exports = {
   config: {
     name: "spy",
-    aliases: ["whoishe", "whoisshe", "whoami", "atake"],
+    aliases: ["spy", "info", "whoami", "atake"],
     version: "1.0",
     role: 0,
     author: "Azad",
