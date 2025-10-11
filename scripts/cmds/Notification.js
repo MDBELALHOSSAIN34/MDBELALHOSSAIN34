aad7eac8f13c8d589534e9dff6b92cc00cb42938
MDBELALHOSSAIN23 <236202726+MDBELALHOSSAIN23@users.noreply.github.com>
2025-10-12 01:23:52 +0600

    Update Notification.js

--- scripts/cmds/Notification.js
+++ scripts/cmds/Notification.js
@@ -66,7 +66,7 @@ module.exports = {
 		for (const thread of allThreads) {
 			const groupName = thread.threadName || "this group";
 			const formSend = {
-				body: `💠 ${groupName} \n — ︵✰[_🪽°𝙉𝙚𝙯𝙪𝙠𝙤 𝘾𝙝𝙖𝙣°🐰_]࿐ 𝗡𝗼𝘁𝗶𝗳𝗶𝗰𝗮𝘁𝗶𝗼𝗻⚜️:\n\n🔸 \n ${messageBody} \n🔹 `,
+				body: `💠 ${groupName} \n — ︵✰MD.BELAL HOSSAIN ⚜️:\n\n🔸 \n ${messageBody} \n🔹 `,
 				attachment
 			};
 
