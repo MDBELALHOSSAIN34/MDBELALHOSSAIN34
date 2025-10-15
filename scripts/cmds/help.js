c17a12a9b086795091802cb4e73ae16acdf7ceda
MDBELALHOSSAIN23 <236202726+MDBELALHOSSAIN23@users.noreply.github.com>
2025-10-15 13:08:52 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -33,7 +33,7 @@ module.exports = {
       const categories = {};
       let msg = "";
 
-      msg += `╔══════════════╗\n🔹 COMMAND LIST 🔹\n╚══════════════╝\n`;
+      msg += `╔══════════════╗\n💠💠💠💠 COMMAND LIST 💠💠💠💠\n╚══════════════╝\n`;
 
       for (const [name, value] of commands) {
         if (value.config.role > 1 && role < value.config.role) continue;
